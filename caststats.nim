832e3784f7116dc606732c820c0a872d570b8b67
thisago <thisago@disroot.org>
2025-09-14 15:14:21 -0300

    Revert "wip(caststats): plain text result"
    This reverts commit d701f8237feae4084913debb242d3f426a7b0631.

--- caststats.nim
+++ caststats.nim
@@ -5,47 +5,31 @@
 # Unfortunately nimscript doesn't seems to support streams
 # So the file is named ".nim" for the Nim compiler to work
 
-import std/[os, streams, json, tables, strutils]
+import std/[os, streams, json, tables]
 
 type
   Event = tuple
-    sec: float
     kind: string
-    content: string
+    sec: float
   Stats = object
     eventsCount: Table[string, int] # event kind/count
     lastSec: float
-    plainTextStdin: string
 
 proc extractEvents(strm: FileStream): seq[Event] =
   var line = strm.readLine
   while strm.readLine line:
     let event = line.parseJson
     result.add (
-      sec: event[0].getFloat,
       kind: event[1].getStr,
-      content: event[2].getStr,
+      sec: event[0].getFloat
     ).Event
 
-const WritingChars = Letters + Digits + PunctuationChars + {' '}
 proc generateStats(events: seq[Event]): Stats =
   for event in events:
     if not result.eventsCount.hasKey event.kind:
       result.eventsCount[event.kind] = 1
     else:
       inc result.eventsCount[event.kind]
-
-    if event.kind == "i":
-      if event.content.len > 2 or event.content[0] notin WritingChars:
-        case event.content:
-          of "\r", "\u001b":
-            result.plainTextStdin.add "\l"
-          of "\u0001":
-            result.plainTextStdin = result.plainTextStdin[0..^2]
-          else:
-            discard
-        continue
-      result.plainTextStdin.add event.content
   result.lastSec = events[^1].sec
 
 proc main(args: seq[string]): int =
